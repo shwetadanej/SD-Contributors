a4ffab203d1ec2e482f5861da4a8d9fefe55f5e6
Shweta Danej <shwetadanej@gmail.com>
2023-12-23 13:34:41 +0530

    Removed unwanted files

--- sd-contributors.php
+++ sd-contributors.php
@@ -18,8 +18,6 @@
  * Version:           1.0.0
  * Author:            Shweta Danej
  * Author URI:        https://shwetadanej.com/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       sd-contributors
  * Domain Path:       /languages
  */
